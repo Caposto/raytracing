e33e67bfa85c759ca79408b1cf7899f61832ed4c
Caposto <christiand.apostol@outlook.com>
2023-01-21 17:08:18 -0500

    antialiasing

--- src/utility.h
+++ src/utility.h
@@ -1,5 +1,5 @@
 #ifndef UTILITY_H
-#define UTILIT_H
+#define UTILITY_H
 
 #include <cmath>
 #include <limits>
@@ -23,6 +23,12 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
 inline double random_double() {
     // Return a random real in [0, 1)
     return rand() / (RAND_MAX + 1.0);
@@ -30,10 +36,10 @@ inline double random_double() {
 
 inline double random_double(double min, double max) {
     // Returns a random real in [min, max)
-    return min + (max - min) * random_double();
+    return min + (max - min)*random_double();
 }
 
-// Common Headerse
+// Common Headers
 
 #include "ray.h"
 #include "vec3.h"
