1fa5d735d94dbe4f17fcb23a17a0ba0245d5991e
Caposto <christiand.apostol@outlook.com>
2023-01-22 15:12:26 -0500

    lambertian material

--- src/vec3.h
+++ src/vec3.h
@@ -55,6 +55,16 @@ class vec3 {
             return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
         }
 
+        bool near_zero() const {
+            // Return true if the vector is close to zero in all directions
+            const auto s = 1e-8;
+            return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+        }
+
+        vec3 reflect(const vec3& v, const vec3& n) {
+            return v - 2*dot(v, n)*n;
+        }
+
     public:
         double e[3];
 };
