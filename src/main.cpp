28806c804838a72f8f997d6cf9e13c35bf302419
Caposto <christiand.apostol@outlook.com>
2023-01-22 14:07:31 -0500

    lambertian

--- src/main.cpp
+++ src/main.cpp
@@ -29,10 +29,12 @@ color ray_color(const ray& r, const hittable& world, int depth) {
         return color(0, 0, 0);
     }
 
-    if(world.hit(r, 0, infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+    // 0.001 instead of 0 to fix shadow acne
+    if(world.hit(r, 0.001, infinity, rec)) {
+        point3 target = rec.p + rec.normal + random_unit_vector();
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
