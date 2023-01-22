14fcb7667ecdd1eae01dd93f820a455fa4251862
Caposto <christiand.apostol@outlook.com>
2023-01-22 16:26:13 -0500

    resetting

--- src/sphere.h
+++ src/sphere.h
@@ -1,20 +1,27 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
+#include "utility.h"
+
 #include "hittable.h"
-#include "vec3.h"
+
 
 class sphere : public hittable {
+    public:
+        sphere() {}
+
+        sphere(point3 cen, double r, shared_ptr<material> m)
+            : center(cen), radius(r), mat_ptr(m) {};
+
+        virtual bool hit(
+            const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
     public:
         point3 center;
         double radius;
         shared_ptr<material> mat_ptr;
-
-        sphere() {}
-        sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {};
-
-        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
 };
+
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
@@ -26,12 +33,12 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     if (discriminant < 0) return false;
     auto sqrtd = sqrt(discriminant);
 
+    // Find the nearest root that lies in the acceptable range.
     auto root = (-half_b - sqrtd) / a;
     if (root < t_min || t_max < root) {
         root = (-half_b + sqrtd) / a;
-        if (root < t_min || t_max < root) {
+        if (root < t_min || t_max < root)
             return false;
-        }
     }
 
     rec.t = root;
@@ -39,8 +46,9 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
     vec3 outward_normal = (rec.p - center) / radius;
     rec.set_face_normal(r, outward_normal);
     rec.mat_ptr = mat_ptr;
-    
+
     return true;
 }
+
 
 #endif
--- src/utility.h
+++ src/utility.h
@@ -2,9 +2,9 @@
 #define UTILITY_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
-#include <cstdlib>
 
 // Usings
 
@@ -30,18 +30,24 @@ inline double clamp(double x, double min, double max) {
 }
 
 inline double random_double() {
-    // Return a random real in [0, 1)
+    // Returns a random real in [0,1).
     return rand() / (RAND_MAX + 1.0);
 }
 
 inline double random_double(double min, double max) {
-    // Returns a random real in [min, max)
-    return min + (max - min)*random_double();
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+inline int random_int(int min, int max) {
+    // Returns a random integer in [min,max].
+    return static_cast<int>(random_double(min, max+1));
 }
 
 // Common Headers
 
 #include "ray.h"
 #include "vec3.h"
+
 
 #endif
