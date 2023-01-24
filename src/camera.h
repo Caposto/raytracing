df89331dd9d3a344d1a71360f2750e6bbfefd312
Caposto <christiand.apostol@outlook.com>
2023-01-24 17:53:27 -0500

    depth of field

--- src/camera.h
+++ src/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "utility.h"
+#include "vec3.h"
 
 class camera {
     public:
@@ -10,7 +11,9 @@ class camera {
             point3 lookat,
             vec3 vup,
             double vfov, // vertical field-of-view in degrees
-            double aspect_ratio
+            double aspect_ratio,
+            double aperture,
+            double focus_dist
         ) {
             auto theta = degrees_to_radians(vfov);
             auto h = tan(theta/2);
@@ -23,13 +26,21 @@ class camera {
             auto v = cross(w, u);
 
             origin = lookfrom;
-            horizontal = viewport_width * u;
-            vertical = viewport_height * v;
-            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
+
+            lens_radius = aperture / 2;
         }
 
         ray get_ray(double s, double t) const {
-            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
+            vec3 rd = lens_radius * random_in_unit_disk();
+            vec3 offset = u * rd.x() + v * rd.y();
+
+            return ray(
+                origin + offset,
+                lower_left_corner + s*horizontal + t*vertical - origin - offset
+            );
         }
 
     private:
@@ -37,6 +48,8 @@ class camera {
         point3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
+        vec3 u, v, w;
+        double lens_radius;
 };
 
 #endif
